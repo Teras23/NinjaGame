f3a893aaef60d0e02fd392eb951348c04c50ad2a
Karl Martin Teras <karlnitram@gmail.com>
2015-03-14 19:46:30 +0200

    AI charge distance

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,28 +10,32 @@ public class Enemy : MonoBehaviour {
     private bool jumping;
     private Vector2 jumpPosition;
     private float jumpTime;
+    public float chargeDistance;
     public float jumpDistance;
     public float jumpSpeed;
+    public float moveSpeed;
 
     private Vector2 playerPosition;
+    private Vector2 position;
 
 	void FixedUpdate () {
         playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        position = new Vector2(transform.position.x, transform.position.y);
 
-        if(!charging && !jumping) {
+        if(!charging && !jumping && (playerPosition - position).magnitude <= chargeDistance) {
             charging = true;
             chargeTime = Time.time;
         }
-
+        else if(!charging && !jumping && (playerPosition - position).magnitude > chargeDistance) {
+            Move();
+        }
+        
         if(charging) {
             Charge();
         }
-
+        
         if(jumping) {
             UpdateAttack();
-        }
-        else {
-            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         }
 
 	}
@@ -62,8 +66,13 @@ public class Enemy : MonoBehaviour {
 
         gameObject.GetComponent<Rigidbody2D>().velocity = (jumpPosition - new Vector2(transform.position.x, transform.position.y)).normalized * jumpSpeed;
     }
-        
+
+    void Move() {
+        gameObject.GetComponent<Rigidbody2D>().velocity = (playerPosition - position).normalized * moveSpeed;
+    }
+
     void Charge() {
+        gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         if(Time.time - chargeTime > chargeDuration) {
             Attack();
             charging = false;
