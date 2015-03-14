00006c1873de04c5524f21da364a2c243f58d7b4
Karl Martin Teras <karlnitram@gmail.com>
2015-03-14 19:09:55 +0200

    Basic AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,13 +4,36 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
     public int hits;
+    public float chargeDuration;
+    private float chargeTime;
+    private bool charging = false;
+    private bool jumping;
+    private Vector2 jumpPosition;
+    private float jumpTime;
+    public float jumpDistance;
+    public float jumpSpeed;
 
-	void Start () {
-	
-	}
-	
-	void Update () {
-	
+    private Vector2 playerPosition;
+
+	void FixedUpdate () {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        if(!charging && !jumping) {
+            charging = true;
+            chargeTime = Time.time;
+        }
+
+        if(charging) {
+            Charge();
+        }
+
+        if(jumping) {
+            UpdateAttack();
+        }
+        else {
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+
 	}
 
     public void Damage() {
@@ -21,6 +44,29 @@ public class Enemy : MonoBehaviour {
         else if(hits == 1) {
             SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
             sr.color = new Color(255, 155, 155);
+        }
+    }
+
+    void Attack() {
+        Vector2 position = transform.position;
+        jumping = true;
+        jumpPosition = position + (playerPosition - position).normalized * jumpDistance;
+        jumpTime = Time.time;
+    }
+
+    void UpdateAttack() {
+        if(Time.time - jumpTime > jumpDistance / jumpSpeed) {
+            jumping = false;
+            return;
+        }
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = (jumpPosition - new Vector2(transform.position.x, transform.position.y)).normalized * jumpSpeed;
+    }
+        
+    void Charge() {
+        if(Time.time - chargeTime > chargeDuration) {
+            Attack();
+            charging = false;
         }
     }
 }
